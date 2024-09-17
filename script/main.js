b7ee611ef5b7e0259a0448b7b90b06c7ab4ae2eb
Julian Domselaar <juliandaniel9809@gmail.com>
2024-09-16 22:38:06 -0300

    adding more changes in my home page

---
+++ script/main.js
@@ -0,0 +1,27 @@
+// Create the global variables
+const year = document.querySelector("#currentyear");
+const modified = document.querySelector("#lastModified");
+
+// I created this arrow funtion only for have the date and hour. Function to format the date
+const formatDate = (date) => {
+    let options = {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    };
+    return date.toLocaleDateString("es-ES", options);
+}
+
+// Use the date object
+const today = new Date();
+const oLastModif = new Date(document.lastModified);
+const formattedDate = formatDate(oLastModif);
+
+// assigning to variable "year" the current date
+year.innerHTML = `${today.getFullYear()}`;
+
+// assigning to variable modified the last modication
+modified.innerHTML = ` Last Modification: ${formattedDate}`;
